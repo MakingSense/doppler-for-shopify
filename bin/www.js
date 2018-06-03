48875a5b231735b78a60f426bc33097335da29c4
Pablo Barrios <pbarrios@makingsense.com>
2018-06-02 21:55:23 -0300

    Add support for HTTPS

--- bin/www.js
+++ bin/www.js
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
 require('dotenv').config();
 const chalk = require('chalk');
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
 const app = require('../server');
-const privateKey = fs.readFileSync('./server/sslcert/server.key', 'utf8');
-const certificate = fs.readFileSync('./server/sslcert/server.crt', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
 
 const port = process.env.SHOPIFY_APP_PORT || '3000';
 app.set('port', port);
 
-const server = http.createServer(app);
+const useHTTPS = (process.env.ENABLE_HTTPS == 'true');
+let server = null;
+
+if (useHTTPS) {
+  const https = require('https');
+  const fs = require('fs');
+  const privateKey = fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH, 'utf8');
+  const certificate = fs.readFileSync(process.env.SSL_CERTIFICATE_KEY_PATH, 'utf8');
+  const credentials = { key: privateKey, cert: certificate };
+  server =  https.createServer(credentials, app);
+}
+else {
+  const http = require('http');
+  server = http.createServer(app);
+}
+
 server.listen(port, err => {
   if (err) {
     return console.log('😫', chalk.red(err));
   }
   console.log(`🚀 Now listening on port ${chalk.green(port)}`);
 });
-
-// const httpsServer = https.createServer(credentials, app);
-
-// httpsServer.listen(port, err => {
-//   if (err) {
-//     return console.log('😫', chalk.red(err));
-//   }
-//   console.log(`🚀 Now listening on port ${chalk.green(port)}`);
-// });

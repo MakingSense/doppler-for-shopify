112ec3995bef7421f55be9742a6bdc0b558c18cd
Pablo Barrios <pbarrios@makingsense.com>
2018-06-03 13:22:49 -0300

    Configure docker to run with prod configuration

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -16,7 +16,6 @@ const plugins = isDevelopment
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production'),
       }),
-      new webpack.optimize.DedupePlugin(),
       new webpack.optimize.UglifyJsPlugin({
         compressor: {
           warnings: false,

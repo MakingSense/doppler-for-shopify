a5ca235dff6cd6ee62d22c623030ac7dcd4b45bc
Pablo Barrios <pbarrios@makingsense.com>
2018-06-24 14:50:06 -0300

    Avoid errors on webhooks

--- server/controllers/hooks.controller.js
+++ server/controllers/hooks.controller.js
@@ -10,10 +10,14 @@ class HooksController {
       return;
     }
 
-    const shopDomain = request.webhook.shopDomain;
+    try
+    {
+      const shopDomain = request.webhook.shopDomain;
 
-    const redis = this.redisClientFactory.createClient();
-    await redis.removeShopAsync(shopDomain, true);
+      const redis = this.redisClientFactory.createClient();
+      await redis.removeShopAsync(shopDomain, true);
+    }
+    catch (err){}
   }
 
   async customerCreated(error, request) {
@@ -21,40 +25,48 @@ class HooksController {
       return;
     }
 
-    const redis = this.redisClientFactory.createClient();
-    const shopInstance = await redis.getShopAsync(
-      request.webhook.shopDomain,
-      true
-    );
+    try
+    {
+      const redis = this.redisClientFactory.createClient();
+      const shopInstance = await redis.getShopAsync(
+        request.webhook.shopDomain,
+        true
+      );
 
-    if (
-      !shopInstance ||
-      !shopInstance.dopplerListId ||
-      !shopInstance.fieldsMapping
-    )
-      return;
+      if (
+        !shopInstance ||
+        !shopInstance.dopplerListId ||
+        !shopInstance.fieldsMapping
+      )
+        return;
 
-    const customer = JSON.parse(request.body);
-    const doppler = this.dopplerClientFactory.createClient(
-      shopInstance.dopplerAccountName,
-      shopInstance.dopplerApiKey
-    );
-    await doppler.createSubscriberAsync(
-      customer,
-      shopInstance.dopplerListId,
-      JSON.parse(shopInstance.fieldsMapping)
-    );
+      const customer = JSON.parse(request.body);
+      const doppler = this.dopplerClientFactory.createClient(
+        shopInstance.dopplerAccountName,
+        shopInstance.dopplerApiKey
+      );
+      await doppler.createSubscriberAsync(
+        customer,
+        shopInstance.dopplerListId,
+        JSON.parse(shopInstance.fieldsMapping)
+      );
+    }
+    catch(err){}
   }
 
   async dopplerImportTaskCompleted(request, response) {
-    const shop = request.query.shop;
-    const redis = this.redisClientFactory.createClient();
-    await redis.storeShopAsync(
-      shop,
-      { synchronizationInProgress: false },
-      true
-    );
-    response.send('Thank you!');
+    try
+    {
+      const shop = request.query.shop;
+      const redis = this.redisClientFactory.createClient();
+      await redis.storeShopAsync(
+        shop,
+        { synchronizationInProgress: false },
+        true
+      );
+      response.send('Thank you!');
+    }
+    catch(err){}
   }
 }
 

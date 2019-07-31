6b3916d63c38f7c18db90233371907bbf3945ea3
Andrés Moschini <andresmoschini@gmail.com>
2019-07-31 13:36:31 -0300

    chore(test): add new factory methods to return Sinon stubed modules

--- test-utilities/modules-mock.js
+++ test-utilities/modules-mock.js
@@ -1,3 +1,5 @@
+const sinon = require('sinon');
+
 const factories = {
   wrappedRedisClient: () => ({
     hmset: () => {},
@@ -47,10 +49,27 @@ const factories = {
     synchronizeCustomers: () => {},
   })
 };
+
+const stubObject = obj => {
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop) && obj[prop] instanceof Function) {
+      sinon.stub(obj, prop);
     }
-}
+  }
+};
+
+const stubFactories = Object.keys(factories).reduce((resultFactories, factoryName) => {
+  resultFactories[factoryName] = () => {
+    const obj = factories[factoryName]();
+    stubObject(obj);
+    return obj;
+  }
+  return resultFactories;
+}, {});
+
 module.exports = {
   factories,
+  stubFactories,
   wrappedRedisClient: factories.wrappedRedisClient(),
   redisClient: factories.redisClient(),
   dopplerClient: factories.dopplerClient(),

a11f00a364019c4932e06abab8b0e830ac106f12
Andrés Moschini <andresmoschini@gmail.com>
2019-08-16 12:24:26 -0300

    feat: accept jwt tokens signed with the new key (and keep fallback with the old one)

--- server/helpers/withDoppler.js
+++ server/helpers/withDoppler.js
@@ -1,9 +1,27 @@
 const jwt = require('jsonwebtoken');
 
 const publicKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA9DH53toQClwPYw5EjN8j
+PUvbmQ9hoi8PxfY5e9bJzoi8mAuNaP2SmmEhP5jmhJYjwwqRRaQex0+HCOO1WG13
+d6FMPCAKvV+rTnKfj59q3LyZ1uXkbk7sTp1m7aSXGAA9MPCuXjuoQNWCyqNFZtJZ
+sRS8BnZMMVfxqKz8uYk6mqrx/cFid7pCz7dfkSFZr2YRSV3xFeVeXfcoPl05xKZO
+3GEp82WRlVa3nlVrqGM1G9odjHLY8mI/jL95p+e+RUPO9aNGUp6RtGfo3vjedWdh
+4/9VDZluqL9weqK/aNc6Z/QnCghuY9BpRnnu9PIbQ7aWT6iMEvYNydUDso7NeKRH
+pQIDAQAB
+-----END PUBLIC KEY-----`;
+
+const oldPublicKey = `-----BEGIN PUBLIC KEY-----
 MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANcuJ2Ukhm7Dhd9ESISn9K31pFvmAZ0Z
 kRLTqQ3a2CPtqM8mwqoH/EU79A0GZtFh+Qn1lrTnzeBw7dINP5yiQFECAwEAAQ==
 -----END PUBLIC KEY-----`;
+
+// It is here only for backwards compatibility for some time, 
+// waiting for Doppler release with the new key. After that, directly 
+// replace it by jwt.verify.
+const verifyJwtWithOldPublicKeyFallback = (token, secretOrPublicKey, options, callback) =>
+  jwt.verify(token, secretOrPublicKey, options, (err, decoded) => 
+    err && err.name == 'JsonWebTokenError' ? jwt.verify(token, oldPublicKey, options, callback)
+    : callback(err, decoded));
 
 const defaultConfiguration = {
   publicKey,
@@ -42,7 +60,7 @@ function withDoppler(configuration) {
       return next();
     } else if (/^ey[\w-]+\.ey[\w-]+\.[\w-]+$/.test(token)) {
       // JWT TOKEN
-      return jwt.verify(token, configuration.publicKey, jwtOptions, (err, decoded) => {
+      return verifyJwtWithOldPublicKeyFallback(token, configuration.publicKey, jwtOptions, (err, decoded) => {
         if (err) {
           res.status(401).send(`Invalid \`Authorization\` token. JWT Error: ${err.message}`);
           return;
